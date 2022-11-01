a143d42c3604e39d611716334b87eba888656cfe
syoungee <sunyoungah69@gmail.com>
2022-11-01 13:10:24 +0900

    feature: 상단 카테고리 UI 구현

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="#000000"
-              stroke-width="3"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <circle cx="11" cy="11" r="8" />
               <line x1="21" y1="21" x2="16.65" y2="16.65" />
@@ -27,6 +27,18 @@ function App() {
               //TODO: 300ms debounce search
             }
           </div>
+        </div>
+        <div className="select-area">
+          {/* TODO: 데이터 가져와서 ul li 구현 */}
+          <div className="category">가격</div>
+          <div className="chip-area">
+            <div className="button">무료</div> <div className="button">유료</div>{' '}
+          </div>
+        </div>
+        <div className="total-count"> 전체 121개 </div>
+        <div className="course-body">
+          {/* TODO: 강의 목록 뿌려주기 */}
+          {/* TODO: pagination 구현 */}
         </div>
       </div>
     </div>
