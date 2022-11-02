db57ca3c7252ed9c2014cb9df47af161e64d9b68
syoungee <sunyoungah69@gmail.com>
2022-11-02 15:00:07 +0900

    feature: card ui 구현 및 페이지네이션 초기 작업

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import { useState, useEffect } from 'react';
 function App() {
   const [filter, setFilter] = useState({ title: '', enroll_type: 0, is_free: true });
   const [price, setPrice] = useState([]);
+  const [pageIndex, setPageIndex] = useState(0);
+  const [courses, setCourses] = useState([]);
+
   const value = {
     filter_conditions: {
       $and: [{ title: `${filter.title}` }, { $or: [{ enroll_type: `${filter.enroll_type}`, is_free: `${filter.is_free}` }] }],
     },
-    offset: 0,
+    offset: pageIndex,
     count: 20,
   };
   // value.filter_conditions.$and.push({ enroll_type: 0, is_free: false });
 
   useEffect(() => {
     getAllCourses(value);
-  }, []);
+  }, [value.offset, price, filter.title]);
 
   const getAllCourses = (data) => {
     getCourses(data).then((res) => {
       if (res.course_count) {
-        console.log(res.courses);
+        setCourses(res.courses);
+        return res.courses;
       }
     });
   };
@@ -37,6 +41,18 @@ function App() {
     } else {
       e.target.classList.add('clicked');
       setPrice([...price, value]);
+    }
+  };
+
+  const pageOnClick = (e) => {
+    const value = e.target.getAttribute('value');
+
+    if (value !== '-1' || value !== '+1') {
+      setPageIndex((parseInt(value) - 1) * 10);
+    } else if (value === '-1') {
+      if (value !== 0) setPageIndex((parseInt(value) - 1) * 10);
+    } else if (value === '+1') {
+      if (value !== 200) setPageIndex((parseInt(value) + 1) * 10);
     }
   };
 
@@ -68,7 +84,6 @@ function App() {
           </div>
         </div>
         <div className="select-area">
-          {/* TODO: 데이터 가져와서 ul li 구현 */}
           <div className="category">가격</div>
           <div className="chip-area">
             <div className="button" value="free" onClick={(e) => onClick(e)}>
@@ -80,17 +95,54 @@ function App() {
           </div>
         </div>
         <div className="total-count"> 전체 121개 </div>
+        {/* TODO: 해당하는 강의 전체 갯수 가져오기 */}
         <div className="course-body">
-          <div className="course-card"></div>
-          <div className="course-card"></div>
-          <div className="course-card"></div>
-          <div className="course-card"></div>
-          <div className="course-card"></div>
-          <div className="course-card"></div>
-          <div className="course-card"></div>
-          <div className="course-card"></div>
-          {/* TODO: 강의 목록 뿌려주기 */}
+          {courses?.map((item, index) => {
+            return (
+              <div className="course-card" id={index}>
+                <div className="course-card-body">
+                  <p className="card-label">유료</p>
+                  <p className="card-title">{item.title}</p>
+                  <p className="card-description">{item.short_description}</p>
+                  <div className="card-icon-container">
+                    <div className="card-icon-text">
+                      <p>{`난이도: ${item.info_summary_visibility_dict.level || '미설정'} `}</p>
+                      <p>{`수업: ${item.info_summary_visibility_dict.level || '온라인'} `}</p>
+                      <p>{`기간: ${item.info_summary_visibility_dict.level || '무제한'} `}</p>
+                    </div>
+                    <img src={item.logo_file_url} className="card-logo" alt="logo_file_url" />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
           {/* TODO: pagination 구현 */}
+        </div>
+
+        <div className="page-container">
+          <span>
+            <div className="index" value="-1" onClick={(e) => pageOnClick(e)}>
+              {`<`}
+            </div>
+            <div className="index" value="1" onClick={(e) => pageOnClick(e)}>
+              1
+            </div>
+            <div className="index" value="2" onClick={(e) => pageOnClick(e)}>
+              2
+            </div>
+            <div className="index" value="3" onClick={(e) => pageOnClick(e)}>
+              3
+            </div>
+            <div className="index" value="4" onClick={(e) => pageOnClick(e)}>
+              4
+            </div>
+            <div className="index" value="5" onClick={(e) => pageOnClick(e)}>
+              5
+            </div>
+            <div className="index" value="+1" onClick={(e) => pageOnClick(e)}>
+              {`>`}
+            </div>
+          </span>
         </div>
       </div>
     </div>
