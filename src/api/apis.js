db32a70369bdcb7ef3baf1d3844bebb95609a0e0
syoungee <sunyoungah69@gmail.com>
2022-11-04 00:50:43 +0900

    add: try catch

--- src/api/apis.js
+++ src/api/apis.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
 export const getCourses = async (data) => {
-  const headers = {
-    'Content-Type': 'application/json',
-  };
-  const result = await axios.get(process.env.REACT_APP_API_URL, { params: JSON.parse(data) }, headers).then((res) => {
-    return res.data;
-  });
-  return result;
+  try {
+    const result = await axios.get(process.env.REACT_APP_API_URL, { params: JSON.parse(data) }).then((res) => {
+      console.log(res.data);
+      return res.data;
+    });
+    return result;
+  } catch (err) {
+    console.log(err);
+    if (err.response.status === 404) {
+      console.log('404 error found');
+    }
+  } finally {
+    console.log('getCourses api called');
+  }
 };
